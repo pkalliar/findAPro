b2d77cd7141dd97c7d77937ed8d5d2c76672f959
pkalliar <pkalliar@yahoo.com>
2017-01-30 23:48:40 +0200

    Create Sender.java

---
+++ Sender.java
@@ -0,0 +1,18 @@
+	public void send(JsonNode toSend, String uri, JsonNode msgId) throws Exception{		
+		httpClient.start();
+		httpClient.newRequest(uri)
+		.method(HttpMethod.POST)
+		.header("APIKEY", apikey)
+		.content(new BytesContentProvider(mapper.writeValueAsBytes(toSend)), "application/octet-stream")
+        .timeout(60, TimeUnit.SECONDS)
+        .send((result) -> { 
+        	System.out.println(" Got response STATUS" +  result.getResponse().getStatus()); 
+        	
+			((ObjectNode) msgId)
+					.put("uri", uri)
+					.put("result", result.getResponse().getStatus())
+					.put("reason", result.getResponse().getReason())
+					.set("msgId", msgId);
+			coord.tell(msgId, meSender);
+        });
+	}
